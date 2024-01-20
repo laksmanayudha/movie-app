1c8b66ebe4f6e8e88605c7e7849a675e2aa0e3b3
laksmanayudha <laksmanayudha22@gmail.com>
2024-01-20 09:17:08 +0800

    add tooltips in cards

--- src/scripts/components/Card/app-card.js
+++ src/scripts/components/Card/app-card.js
@@ -1,3 +1,5 @@
+import { Tooltip } from 'bootstrap';
+
 class AppCard extends HTMLElement {
   set card(card) {
     this._card = card;
@@ -20,12 +22,25 @@ class AppCard extends HTMLElement {
         <img src="${image}" class="card-img img-fit" alt="movie image">
         <div class="card-img-overlay backdrop text-white">
           <div class="h-100 d-flex flex-column justify-content-end">
-            <h5 class="card-title text-ellipsis">${title}</h5>
-            <p class="card-text fw-light">${description}</p>
+            <h5
+              class="card-title text-ellipsis"
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              data-bs-title="${title}"
+            >${title}</h5>
+            <p
+              class="card-text fw-light text-ellipsis"
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              data-bs-title="${description}"
+            >${description}</p>
           </div>
         </div>
       </div>
     `;
+
+    const tooltips = this.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltips].forEach((tooltip) => new Tooltip(tooltip));
 
     this.onclick = async () => {
       await onclick(this);
