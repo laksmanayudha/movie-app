772fff968050bfd62a8391849bc14a66d3b90c37
laksmanayudha <laksmanayudha22@gmail.com>
2024-01-21 13:41:35 +0800

    move dotenv webpack to dev

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
@@ -88,7 +87,5 @@ module.exports = {
         { from: './src/assets/img', to: path.resolve(__dirname, 'dist', 'assets', 'img') },
       ],
     }),
-    // ENV
-    new Dotenv(),
   ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
 const { merge } = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  plugins: [
+    // ENV
+    new Dotenv(),
+  ],
 });
